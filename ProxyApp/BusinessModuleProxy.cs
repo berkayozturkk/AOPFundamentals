1de73fc5ff32a9c85ca03a71a57608c9b91eb72e
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-07-18 22:38:11 +0300

    Add project files.

---
+++ ProxyApp/BusinessModuleProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProxyApp
+{
+    public class BusinessModuleProxy : IBusinessModule
+    {
+        private BusinessModule _realObjext;
+
+        public BusinessModuleProxy()
+        {
+            _realObjext = new BusinessModule();
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Before method.");
+
+            _realObjext.Method();
+
+            Console.WriteLine("End method.");
+        }
+    }
+}
---
+++ ProxyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessModuleProxy _businessModule = new BusinessModuleProxy();
+            _businessModule.Method();
+
+            Console.ReadLine();
+        }
+    }
+}
