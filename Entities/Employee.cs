9e4ee23ac2e2d10aaef80f5f964841990645684c
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-07-18 22:56:56 +0300

    Create Entities and Invocation project.Create Employee

---
+++ Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual void Add(int id,string firstName,string lastName)
+        {
+            Console.WriteLine("Method body start");
+
+            Console.WriteLine($"\t Id : {Id}");
+            Console.WriteLine($"\t firstName : {firstName}");
+            Console.WriteLine($"\t lastName : {lastName}");
+
+            Console.WriteLine("Method body end");
+
+        }
+    }
+}
